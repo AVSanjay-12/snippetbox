198f761a5b32131bafcd349ce2c5b22711a0fbce
Sanjay A V <sanjayav1262@gmail.com>
2024-09-23 12:30:23 +0530

    DB connection and operation

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,12 +7,14 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/AVSanjay-12/snippetbox/internal/models"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct{
 	errorLog *log.Logger
 	infoLog *log.Logger
+	snippets *models.SnippetModel
 }
 
 func main() {
@@ -37,6 +39,7 @@ func main() {
 	app := &application{
 		errorLog: errorLog,
 		infoLog: infoLog,
+		snippets: &models.SnippetModel{DB: db},
 	}
 
 	srv := &http.Server{
