5df728aa75cdf1e11f11f3c8775dcf05b9872554
Sanjay A V <sanjayav1262@gmail.com>
2024-10-27 18:39:57 +0530

    Added middleware for security header and log request

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,7 +2,7 @@ package main
 
 import "net/http"
 
-func (app *application) routes() *http.ServeMux{
+func (app *application) routes() http.Handler{
 	mux := http.NewServeMux()
 
 	// Filer server for using static files from disk
@@ -14,5 +14,5 @@ func (app *application) routes() *http.ServeMux{
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
-	return mux
+	return app.logRequest(secureHeaders(mux))
 }
