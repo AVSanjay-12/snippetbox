ac5607b504f106bb869d4883b1e0538daee747fc
Sanjay A V <sanjayav1262@gmail.com>
2024-11-26 10:29:27 +0530

    Implemented httprouter

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,22 +3,32 @@ package main
 import (
 	"net/http"
 
+	"github.com/julienschmidt/httprouter"
 	"github.com/justinas/alice"
 )
 
-func (app *application) routes() http.Handler{
-	mux := http.NewServeMux()
+func (app *application) routes() http.Handler {
+	// Initialize
+	router := httprouter.New()
 
-	// Filer server for using static files from disk
+	router.NotFound = http.HandlerFunc(func (w http.ResponseWriter, r *http.Request)  {
+		app.notFound(w)			
+	})
+
+	// File server for serving static files from disk
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
 
+	// Application routes
+	// router.HandlerFunc is an adapter -> Allows the usage of http.HandlerFunc
+	// as a request handle
+	router.HandlerFunc(http.MethodGet, "/", app.home)
+	router.HandlerFunc(http.MethodGet, "/snippet/view/:id", app.snippetView)
+	router.HandlerFunc(http.MethodGet, "/snippet/create", app.snippetCreate)
+	router.HandlerFunc(http.MethodPost, "/snippet/create", app.snippetCreatePost)
 
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
-
+	// Middleware chaining
 	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 
-	return standard.Then(mux)
+	return standard.Then(router)
 }
