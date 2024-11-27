1c5e3b23e203e3e3cf1d17fc362f30428a282296
Sanjay A V <sanjayav1262@gmail.com>
2024-11-27 08:48:33 +0530

    Validate the form data

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
+	"unicode/utf8"
 
 	"github.com/AVSanjay-12/snippetbox/internal/models"
 	"github.com/julienschmidt/httprouter"
@@ -72,6 +74,27 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 	expires, err := strconv.Atoi(r.PostForm.Get("expires"))
 	if err != nil{
 		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	fieldErrors := make(map[string]string)
+
+	if strings.TrimSpace(title) == ""{
+		fieldErrors["title"] = "The title should not be empty"
+	} else if utf8.RuneCountInString(title) > 100{
+		fieldErrors["content"] = "The title should be less 100 characters long"
+	}
+
+	if strings.TrimSpace(content) == ""{
+		fieldErrors["title"] = "The content should not be empty"
+	} 
+
+	if expires != 1 && expires != 7 && expires != 365{
+		fieldErrors["expires"] = "This field must equal 1, 7 or 365"
+	}
+
+	if len(fieldErrors) > 0{
+		fmt.Fprint(w, fieldErrors);
 		return
 	}
 
