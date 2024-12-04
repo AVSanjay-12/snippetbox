bafdb9c16ae3a0856cad8881d930ee7049ee4282
Sanjay A V <sanjayav1262@gmail.com>
2024-12-04 21:07:41 +0530

    Update to HTTPS

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -56,6 +56,7 @@ func main() {
 	sessionManager := scs.New()
 	sessionManager.Store = mysqlstore.New(db)
 	sessionManager.Lifetime = 12 * time.Hour
+	sessionManager.Cookie.Secure = true
 
 	// New instance of application struct - contains dependencies
 	app := &application{
@@ -74,7 +75,7 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
-	err = srv.ListenAndServe()
+	err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 	errorLog.Fatal(err)
 }
 
