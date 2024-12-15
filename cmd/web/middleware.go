22ff520a8b2f226b37f369d80c30eca36e114b19
Sanjay A V <sanjayav1262@gmail.com>
2024-12-15 07:30:00 +0530

    Add Test for SecureHeader Middleware

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -14,7 +14,7 @@ func secureHeaders(next http.Handler) http.Handler{
 		w.Header().Set("X-Content-Type-Options", "nosniff")
 		w.Header().Set("X-Frame-Options", "deny")
 		w.Header().Set("X-XSS-Protection", "0")
-
+		w.Header().Set("Referrer-Policy","origin-when-cross-origin")
 		next.ServeHTTP(w, r)
 	})
 }
--- cmd/web/templates_test.go
+++ cmd/web/templates_test.go
@@ -3,6 +3,8 @@ package main
 import (
 	"testing"
 	"time"
+
+	"github.com/AVSanjay-12/snippetbox/internal/assert"
 )
 func TestHumanDate(t *testing.T) {
 	// Initialize a new time.Time object and pass it to the humanDate function.
@@ -30,9 +32,7 @@ func TestHumanDate(t *testing.T) {
 	for _, tt:= range tests{
 		t.Run(tt.name, func(t *testing.T){
 			hd := humanDate(tt.tm)
-			if hd != tt.want {
-				t.Errorf("got %q; want %q", hd, tt.want)
-			}
+			assert.Equal(t, hd, tt.want)
 		})
 	}
 
